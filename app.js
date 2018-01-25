857ce9ebba66a1f7f1a2964e19ef26bbcdbedf45
Ozan Sazak <ozan.sazak@protonmail.ch>
2018-01-26 02:17:49 +0300

    Initial comment

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Ozan',
-    job: 'işsiz'
+    age: 18,
+    x: 0,
+    y: 0
   },
   methods: {
-    greet: function(time){
-      console.log(this);
-      return 'Good ' + time + ', ' + this.name + ' the ' + this.job;
+    add(n){
+      n = n || 1;
+      this.age += n;
+    },
+    sub(n){
+      n = n || 1;
+      this.age -= n;
+    },
+    updateXY(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
